c2a47436c54b4e72e5ff62be0527a104638f6c07
Mikuasovsky <120600479+Mikuasovsky@users.noreply.github.com>
2024-06-11 01:05:15 +0100

    Update

--- login_page/login_page.js
+++ login_page/login_page.js
@@ -5,3 +5,27 @@ menu.addEventListener('click', function () {
   menu.classList.toggle('is-active')
   menuLinks.classList.toggle('active')
 });
+
+const loginBox = document.querySelector('.login-box')
+const loginLink = document.querySelector('.login-link')
+const registerLink = document.querySelector('.register-link')
+const btnPopup = document.querySelector('.btnLogin-popup')
+const iconClose = document.getElementById('iconClose')
+
+
+
+registerLink.addEventListener('click',  () => {
+  loginBox.classList.add('active');
+});
+
+loginLink.addEventListener('click',  () => {
+  loginBox.classList.remove('active');
+});
+
+btnPopup.addEventListener('click',  () => {
+  loginBox.classList.add('active-popup');
+});
+
+iconClose.addEventListener('click',  () => {
+  loginBox.classList.remove('active-popup');
+});
