6ee902f37c1ca226bfce4b9d96cbae82ca9ff646
Mikuasovsky <120600479+Mikuasovsky@users.noreply.github.com>
2024-06-16 12:27:31 +0100

    js

--- login_page/login_page.js
+++ login_page/login_page.js
@@ -1,5 +1,11 @@
 const menu = document.querySelector('#mobile-menu')
 const menuLinks = document.querySelector('.navbar_menu')
+let txtemail = document.querySelector('.txt-email')
+let txtpass = document.querySelector('.txt-pass')
+let submit = document.querySelector('.btn-sub')
+
+
+
 
 menu.addEventListener('click', function () {
   menu.classList.toggle('is-active')
@@ -18,3 +24,25 @@ registerLink.addEventListener('click',  () => {
 loginLink.addEventListener('click',  () => {
   loginBox.classList.remove('active');
 });
+
+if(window.localStorage){
+  //console.log("Supported...")
+  localStorage.setItem("email", "admin@example.com");
+  localStorage.setItem("pass", "admin123");
+  
+  let email = localStorage.getItem("email");
+  let pass = localStorage.getItem("pass");
+
+  let message = localStorage.getItem(".message");
+
+  submit.addEventListener('click', () => {
+    if(email == txtemail.value && pass == txtpass.value){
+      message.innerHTML ="Login Successfull!";
+    }else{
+      message.innerHTML ="Username or Password Invalid!";
+    }
+  })
+
+}else{
+  console.log("Not Supported...")
+}
